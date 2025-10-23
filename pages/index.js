6b785cabe4ccff43ebdac67199f61599d8f83e64
RodneyRicardo <113321898+RodneyRicardo@users.noreply.github.com>
2025-10-23 18:06:03 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Vou fazer 55 anos no dia 23 de outubro, esse curso esta me dando a chance de sonhar novamente com a área de tecnologia</h1>
+}
+
+export default Home;
