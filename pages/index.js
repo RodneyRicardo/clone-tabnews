2257bd1f8c5361677aa70d786429bea0945927c8
RodneyRicardo <113321898+RodneyRicardo@users.noreply.github.com>
2025-10-25 00:34:55 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Vou fazer 55 anos no dia 23 de outubro, esse curso esta me dando a chance de sonhar novamente com a área de tecnologia</h1>
+    return <h1>Vou fazer 55 anos no dia 23 de outubro, 
+        esse curso esta me dando a chance de sonhar novamente com a área de tecnologia!
+        Salve!!! </h1>
 }
 
 export default Home;
