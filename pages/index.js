751cbbd31f9108f2f99648d4cefff063f3d2907f
RodneyRicardo <113321898+RodneyRicardo@users.noreply.github.com>
2025-11-01 23:52:14 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-function Home(){
-    return <h1>Vou fazer 55 anos no dia 23 de outubro, 
-        esse curso esta me dando a chance de sonhar novamente com a área de tecnologia!
-        Salve!!! </h1>
+function Home() {
+  return (
+    <h1>
+      Vou fazer 55 anos no dia 23 de outubro, esse curso esta me dando a chance
+      de sonhar novamente com a área de tecnologia! Salve!!!{" "}
+    </h1>
+  );
 }
 
 export default Home;
